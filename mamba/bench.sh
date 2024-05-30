60d3e2a48284e127991ed9ea988f39d5089f2a8e
Ben Moss <bmoss40@bloomberg.com>
2024-05-30 12:10:17 -0400

    fix prepare

--- mamba/bench.sh
+++ mamba/bench.sh
@@ -2,4 +2,4 @@
 
 set -euxo pipefail
 
-hyperfine --warmup 1 --prepare "rm -rf ../bench/envs/mamba" "micromamba env create -r $CACHE_DIR -n mamba -f mamba/environment-lock.yml"
+hyperfine --warmup 1 --prepare "rm -rf $CACHE_DIR/envs/mamba" "micromamba env create -r $CACHE_DIR -n mamba -f mamba/environment-lock.yml"
