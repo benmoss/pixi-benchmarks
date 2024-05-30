8d32ae691bc493c7d3f4ecd2d8fecf97af228e58
Ben Moss <bmoss40@bloomberg.com>
2024-05-30 12:25:38 -0400

    Add benchmark output

--- mamba/bench.sh
+++ mamba/bench.sh
@@ -2,4 +2,4 @@
 
 set -euxo pipefail
 
-hyperfine --warmup 1 --prepare "rm -rf $CACHE_DIR/envs/mamba" "micromamba env create -r $CACHE_DIR -n mamba -f mamba/environment-lock.yml --yes"
+hyperfine --export-markdown mamba.md --warmup 1 --prepare "rm -rf $CACHE_DIR/envs/mamba" "micromamba env create -r $CACHE_DIR -n mamba -f mamba/environment-lock.yml --yes"
--- pixi/bench.sh
+++ pixi/bench.sh
@@ -6,4 +6,4 @@ pushd pixi
 
 export PIXI_CACHE_DIR="$CACHE_DIR/pixi"
 export PIXI_PROJECT_MANIFEST=$PWD/pixi.toml
-hyperfine --warmup 1 --prepare "rm -rf .pixi" "pixi install"
+hyperfine --export-markdown ../pixi.md --warmup 1 --prepare "rm -rf .pixi" "pixi install"
